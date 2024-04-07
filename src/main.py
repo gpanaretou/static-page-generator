c47ee820b6bc7cf2e0c39905490db54d8d604f69
gpanaretou <gpanaretou@hotmail.com>
2024-04-07 14:33:02 +0300

    progress

--- src/main.py
+++ src/main.py
@@ -1,7 +1,56 @@
 from textnode import TextNode
 from htmlnode import HtmlNode, LeafNode, ParentNode
 
+from enum import Enum
 
+class TextType(Enum):
+    text = ''
+    bold = '**'
+    italic = '*'
+    code = '`'
+    link = ''
+    image = ''
+
+def text_node_to_html_node(text_node):
+    text_type = text_node.text_type
+
+    if text_type not in TextType.__members__.keys():
+        raise ValueError(f"{text_type} is not supported!")
+
+    if text_type == TextType.text.name:
+        return LeafNode(value=text_node.text)
+    if text_type == TextType.bold:
+        return LeafNode(tag="b", value=text_node.text)
+    if text_type == TextType.italic:
+        return LeafNode(tag="i", value=text_node.text)
+    if text_type == TextType.code:
+        return LeafNode(tag="code", value=text_node.text)
+    if text_type == TextType.link:
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    if text_type == TextType.image:
+        return LeafNode(tag="img", value="", props={"src": text_node.url,
+                                                    "alt": text_node.text})
+
+def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: str) -> list:
+    new_nodes = []
+    
+    if (delimiter == '`' and text_type is not TextType.code.name) or \
+        (delimiter == '**' and text_type is not TextType.bold.name) or \
+        (delimiter == '*' and text_type is not TextType.italic.name):
+        raise Exception("This delimiter is not supported by Markdown!")
+
+    for node in old_nodes:
+        if (node.text_type != TextType.text.name):
+            new_nodes.append(node)
+            continue
+
+        split_text = node.text.split(f"{delimiter}")
+        for text in split_text:
+            if text[-1] != ' ':
+                new_nodes.append(TextNode(text, text_type))
+            else:
+                new_nodes.append(TextNode(text, 'text'))
+    return new_nodes
 
 def main():
     print("*** HI ***\n")
@@ -39,8 +88,16 @@ def main():
     # print(nested_node.to_html())
 
     text_node = TextNode('test', 'text')
-    html_node = TextNode.text_node_to_html_node(text_node)
+    html_node = text_node_to_html_node(text_node)
     print(html_node)
+
+    node1 = TextNode("This is text with a `code block` word", 'text')
+    node2 = TextNode("This is text with a `code block` word", 'text')
+
+
+    new_nodes = split_nodes_delimiter([node1, node2], "`", TextType.code.name)
+
+    print(new_nodes)
     print("\n*** ByE ***")
 
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,14 +1,4 @@
-from enum import Enum
 from htmlnode import LeafNode
-
-
-class TextType(Enum):
-    text = 1
-    bold = 2
-    italic = 3
-    code = 4
-    link = 5
-    image = 6
 
 
 class TextNode:
@@ -24,22 +14,3 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
-    def text_node_to_html_node(text_node):
-        text_type = text_node.text_type
-
-        if text_type not in TextType.__members__.keys():
-            raise ValueError(f"{text_type} is not supported!")
-
-        if text_type == TextType.text.name:
-            return LeafNode(value=text_node.text)
-        if text_type == TextType.bold:
-            return LeafNode(tag="b", value=text_node.text)
-        if text_type == TextType.italic:
-            return LeafNode(tag="i", value=text_node.text)
-        if text_type == TextType.code:
-            return LeafNode(tag="code", value=text_node.text)
-        if text_type == TextType.link:
-            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
-        if text_type == TextType.image:
-            return LeafNode(tag="img", value="", props={"src": text_node.url,
-                                                        "alt": text_node.text})
