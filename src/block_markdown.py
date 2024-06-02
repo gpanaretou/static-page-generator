369e31bf75da32989684952ec1c98c936e8615fd
George Panaretou <gpanaretou@hotmail.com>
2024-06-02 15:39:34 +0300

    add tests for markdown to block - found a bug with split_delimiter. if last char is part of the delimiters it will remove it from the from the final markdown.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -19,7 +19,7 @@ def markdown_to_blocks(markdown):
     blocks = [x.strip() for x in blocks]
     return blocks
 
-def block_to_block_type(block):
+def get_block_type(block):
     first_char = block[0]
     
     if first_char == BlockType.heading.value:
@@ -30,7 +30,7 @@ def block_to_block_type(block):
         return BlockType.unordered_list.name
     elif first_char == BlockType.unordered_list_dash.value:
         return BlockType.unordered_list.name
-    elif first_char == BlockType.code.value:
+    elif first_char == BlockType.code.value and block[-1] == BlockType.code.value:
         return BlockType.code.name
         
     second_char = block[1]
@@ -44,20 +44,23 @@ def block_to_block_type(block):
     else:
         return BlockType.paragraph.name
 
-def handle_paragraph_block(block, nodes: list):
+def handle_paragraph_block(nodes: list):
     parent_node = ParentNode(tag='p', children=nodes)
     return parent_node
 
-def handle_quote_block(block, nodes: list):
+def handle_quote_block(nodes: list):
+    for node in nodes:
+        node.value = node.value.strip('> ')
+
     parent_node = ParentNode(tag='blockquote', children=nodes)
     return parent_node
 
-def handle_code_block(block, nodes: list):
+def handle_code_block(nodes: list):
     code_node = ParentNode(tag='code', children=nodes)
     parent_node = ParentNode(tag='pre', children=code_node)
     return parent_node
 
-def handle_ul_node(block, nodes: list):
+def handle_ul_node(nodes: list):
     for node in nodes:
         node.tag = 'li'
         node.value = node.value.strip('*- ')
@@ -65,10 +68,11 @@ def handle_ul_node(block, nodes: list):
     parent_node = ParentNode(tag='ul', children=nodes)
     return parent_node
 
-def handle_ol_node(block, nodes: list):
+def handle_ol_node(nodes: list):
+    pattern = r'^\d+\.\s*'
     for node in nodes:
         node.tag = 'li'
-        node.value = node.value.strip('*- ')
+        node.value = re.sub(pattern, '', node.value)
 
     parent_node = ParentNode(tag='ol', children=nodes)
     return parent_node  
@@ -78,7 +82,9 @@ def wrap_node_with_div(html_node):
     return parent_node
 
 
-def wrap_block_with_appropriate_html_node(block_type: str, block):
+def block_to_html(block):
+
+    block_type = get_block_type(block)
     html_nodes = []
 
     text_nodes = []
@@ -89,26 +95,22 @@ def wrap_block_with_appropriate_html_node(block_type: str, block):
         html_nodes.append(node[0].text_node_to_html_node())
 
     if block_type == BlockType.paragraph.name:
-        return handle_paragraph_block(block, html_nodes)
+        return handle_paragraph_block(html_nodes)
     elif block_type == BlockType.code.name:
-        return handle_code_block(block, html_nodes)
+        return handle_code_block(html_nodes)
     elif block_type == BlockType.quote.name:
-        return handle_quote_block(block, html_nodes)
+        return handle_quote_block(html_nodes)
     elif block_type == BlockType.unordered_list.name:
-        return handle_ul_node(block, html_nodes)
+        return handle_ul_node(html_nodes)
+    elif block_type == BlockType.ordered_list.name:
+        return handle_ol_node(html_nodes)
 
-def markdown_to_html_node(markdown):
+def markdown_to_html(markdown):
     blocks = markdown_to_blocks(markdown)
 
-    for i, block in enumerate(blocks):
-        # print(i, " -> ", block)
-        pass
-
     for block in blocks:
-        block_type = block_to_block_type(block)
-
-        parent_node = wrap_block_with_appropriate_html_node(block_type, block)
+        parent_node = block_to_html(block)
         parent_node = wrap_node_with_div(parent_node)
-    return parent_node
+    return parent_node.to_html()
 
         
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -21,7 +21,7 @@ This is the same paragraph on a new line
         
         self.assertEqual(actual, expected)
 
-    def test_block_to_block_type(self):
+    def test_get_block_type(self):
         blocks = ['>This is **bolded** paragraph',
                     'This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line',
                     "* This is a list\n* with items",
@@ -33,12 +33,11 @@ This is the same paragraph on a new line
         expected = ["quote", "paragraph", "unordered_list", "heading", "ordered_list", 'code']
 
         for i, block in enumerate(blocks):
-            actual = block_to_block_type(block)
+            actual = get_block_type(block)
 
             self.assertEqual(expected[i], actual)
 
     def test_handle_paragraph_block(self):
-        block = "This is a simple block of text"
         nodes = [
             LeafNode("b", "Bold text"),
             LeafNode(None, "Normal text"),
@@ -46,28 +45,107 @@ This is the same paragraph on a new line
             LeafNode(None, "Normal text"),
         ]
 
-        new_node = handle_paragraph_block(block, nodes)
+        new_node = handle_paragraph_block(nodes)
         expected_node = ParentNode(tag="p", children=nodes)
         self.assertEqual(expected_node, new_node)
 
     def test_handle_quote_block(self):
-        block = ">This is a simple block of text"
         nodes = [
             LeafNode('span', "This is a simple block of text")
         ]
 
-        new_node = handle_quote_block(block, nodes)
+        new_node = handle_quote_block(nodes)
         expected_node = ParentNode(tag="blockquote", children=nodes)
         self.assertEqual(expected_node, new_node)
 
     def test_handle_code_block(self):
-        block = "```This is a simple block of text```"
         nodes = [
             LeafNode("code", "This is a simple block of text")
         ]
 
-        new_node = handle_code_block(block, nodes)
+        new_node = handle_code_block(nodes)
         expected_node = ParentNode(tag="pre", children=ParentNode('code', nodes))
         self.assertEqual(expected_node, new_node)
 
-    # def test_handle_ul_node(self):
+    def test_handle_ul_node(self):
+        nodes = [
+            LeafNode('li', 'First node'),
+            LeafNode('li', 'Second node')
+        ]
+        new_node = handle_ul_node(nodes)
+        expected_node = ParentNode(tag='ul', children=nodes)
+
+        self.assertEqual(expected_node, new_node)
+
+    def test_handle_ol_node(self):
+        nodes = [
+            LeafNode('li', 'First node'),
+            LeafNode('li', 'Second node')
+        ]
+        new_node = handle_ol_node(nodes)
+        expected_node = ParentNode(tag='ol', children=nodes)
+
+        self.assertEqual(expected_node, new_node)
+
+    def test_wrap_node_with_div(self):
+        nodes = [
+            LeafNode('li', 'First node'),
+            LeafNode('li', 'Second node')
+        ]
+        new_node = wrap_node_with_div(nodes)
+        expected_node = ParentNode('div', nodes)
+        self.assertEqual(expected_node, new_node)
+
+    def test_block_to_html_with_paragraph_block(self):
+        paragraph_block = "This is the first line\nThis is the second line"
+        expected_result = ParentNode(
+            'p',
+            [LeafNode(value="This is the first line"), LeafNode(value="This is the second line")]
+        )
+
+        actual_result = block_to_html(paragraph_block)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_block_to_html_with_quote_block(self):
+        quote_block = ">This is the first line\n>This is the second line"
+        expected_result = ParentNode(
+            'blockquote',
+            [LeafNode(value="This is the first line"), LeafNode(value="This is the second line")]
+        )
+        actual_result = block_to_html(quote_block)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_block_to_html_with_code_block(self):
+        block = "`This is the first line\nThis is the second line`"
+        expected_result = ParentNode(
+            'pre',
+            ParentNode('code', [LeafNode(value="This is the first line"), LeafNode(value="This is the second line")])
+        )
+        actual_result = block_to_html(block)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_block_to_html_with_ul_block(self):
+        block = "* This is the first line\n* This is the second line"
+        expected_result = ParentNode(
+            'ul',
+            [LeafNode('li', value="This is the first line"), LeafNode('li', value="This is the second line")]
+        )
+        actual_result = block_to_html(block)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_block_to_html_with_ol_block(self):
+        block = "1. This is the first line\n2. This is the second line"
+        expected_result = ParentNode(
+            'ol',
+            [LeafNode('li', value="This is the first line"), LeafNode('li', value="This is the second line")]
+        )
+        actual_result = block_to_html(block)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_markdown_to_html(self):
+        block = "1. This is the first line\n2. This is the second line"
+        result = markdown_to_html(block)
+        expected = '<div><ol><li>This is the first line</li><li>This is the second line</li></ol></div>'
+
+        self.assertEqual(expected, result)
+    
