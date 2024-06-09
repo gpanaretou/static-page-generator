47efcad53cdb3ed310e30867c5c4a9465ee7f8f2
George Panaretou <gpanaretou@hotmail.com>
2024-06-09 13:24:49 +0300

    add recursive directory copy from static to public

--- src/main.py
+++ src/main.py
@@ -7,42 +7,38 @@ from inline_markdown import *
 from block_markdown import *
 
 
-def rec_traverse_dir(path):
-    # if path.
-    next
+def recursively_copy_directories(src, dst, space=0):
+    spaces = space * '  '
+    if os.path.isfile(src):
+        print(f"{spaces}-> Copying {src} into {dst}")
+        shutil.copy(src, dst)
+        return
+
+    for x in os.listdir(src):
+        if os.path.isfile(os.path.join(src, x)):
+            print(f"{spaces}-> Copying {x} into {dst}")
+            shutil.copy(os.path.join(src, x), dst)
+        else:
+            print(f"{spaces}-> Creating {os.path.join(dst, x)} directory")
+            os.mkdir(os.path.join(dst, x))
+            recursively_copy_directories(os.path.join(src, x), os.path.join(dst, x), space=space+1)
 
 def copy_from_static_to_public():
-    if os.path.exists('static'):
-
-        print('------')
-
-        if os.path.exists('public'):
-            print('-> Deleting public folder.')
-            shutil.rmtree('public')
-        else:
-            print('-> Creating public folder.')
-            os.mkdir('public')
-
-        root_path = 'static'
-        dst_root = 'public'
-
-        for x in os.listdir('static'):
-            path = os.path.join(root_path, x)
-            dst_path = os.path.join(dst_root, x)
-
-            if os.path.isfile(path):
-                print(f"-> Copying {path} into {dst_path}")
-                shutil.copy(path, dst_path)
-            else:
-                print("-> Creating {dst_path} directory")
-                os.mkdir(dst_path)
-
-        
-
-        
-
-    else:
+    if os.path.exists('static') == False:
         print('Was expecting to find a directory with the name \'static\'.')
+
+    print('------')
+
+    if os.path.exists('public'):
+        print('-> Deleting public folder.')
+        shutil.rmtree('public')
+        print('-> Creating public folder.')
+        os.mkdir('public')
+    else:
+        print('-> Creating public folder.')
+        os.mkdir('public')
+
+    recursively_copy_directories(os.path.join('static'), os.path.join('public'))
 
 def main():
     print("*** HI ***\n")
